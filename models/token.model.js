7961b6a8c1e53a9cdb8ce39c6689582ac42a5966
SoulNinja-dev <aadhi123awesome@gmail.com>
2022-01-16 22:12:40 +0530

    chore: model

---
+++ models/token.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const TokenSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  attributes: [
+    {
+      trait_type: {
+        type: String,
+        required: true,
+        enum: ["Eyes", "Ears", "Hair", "Sex"],
+      },
+      value: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  image_url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+export default TokenSchema;
