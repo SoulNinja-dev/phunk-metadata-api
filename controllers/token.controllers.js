2e7352b63409dcd3ec2ae7fc9aaea62463a97217
SoulNinja-dev <aadhi123awesome@gmail.com>
2022-01-17 17:26:16 +0530

    chore: token controllers

--- controllers/token.controllers.js
+++ controllers/token.controllers.js
@@ -1,13 +1,27 @@
 // getTokens, getToken
 import TokenModel from "../models/token.model.js";
 
-const getTokens = (req, res, next) => {
-  res.status(200).json({ status: "ok", data: {} });
+const getTokens = async (req, res, next) => {
+  // TODO: advanced results middleware for pagination and select
+  const tokens = await TokenModel.find({}).catch((e) => {
+    console.error(e);
+    res.status(500).json({ status: "error", error: e });
+  });
+  res.status(200).json({ status: "ok", data: tokens });
 };
 
-const getToken = (req, res, next) => {
-  console.log(req.params.tokenid);
-  res.status(200).json({ status: "ok", data: {} });
+const getToken = async (req, res, next) => {
+  const tokenid = req.params.tokenid;
+  const token = await TokenModel.find({ tokenid: tokenid }).catch((e) => {
+    console.log(e);
+    res.status(500).json({ status: "error", error: e });
+  });
+  if (token.length < 1) {
+    return res
+      .status(400)
+      .json({ status: "error", error: `No token with id ${tokenid}` });
+  }
+  res.status(200).json({ status: "ok", data: token });
 };
 
 export { getTokens, getToken };
--- routes/tokens.routes.js
+++ routes/tokens.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
-const router = express.Router();
 import { getTokens, getToken } from "../controllers/token.controllers.js";
+
+const router = express.Router();
 
 router.get("/", getTokens);
 router.get("/:tokenid", getToken);
