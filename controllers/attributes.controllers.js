a2b6ba0e835ed308c619423c9a7f44c164ceb5f0
SoulNinja-dev <aadhi123awesome@gmail.com>
2022-01-19 00:19:38 +0530

    chore: pagination for attributes

--- controllers/attributes.controllers.js
+++ controllers/attributes.controllers.js
@@ -10,16 +10,69 @@ const attributes = async (req, res, next) => {
       .status(400)
       .json({ status: "error", error: `No trait type found` });
   }
-  console.log(trait_type);
-  const tokens = await TokenModel.find({
+  let query;
+  const reqQuery = { ...req.query };
+
+  const removeQueries = ["select", "page", "value"];
+
+  // removing the unnecesary queries
+  removeQueries.forEach((query) => delete reqQuery[query]);
+  query = TokenModel.find({
     attributes: {
       $elemMatch: {
         trait_type: trait_type,
       },
     },
   });
-  console.log(tokens);
-  res.status(200).json({ status: "ok", data: { tokens } });
+
+  // selecting particular fields query
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = 50;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await TokenModel.countDocuments();
+  query = query.skip(startIndex).limit(limit);
+
+  const tokens = await query.catch((e) => {
+    console.log(e);
+    return res.status(400).json({ status: "error", error: e });
+  });
+
+  if (tokens.length < 1) {
+    return res
+      .status(400)
+      .json({ status: "error", error: "Invalid request, check parameters" });
+  }
+
+  // pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: tokens.length,
+    pagination: pagination,
+    data: tokens,
+  });
 };
 
 export { attributes };
